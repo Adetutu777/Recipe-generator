c951baf7720116453fed8d2476d82be8f49e4cc0
Adetutu Gbangbola <adetutugbangbola@gmail.com>
2020-08-16 19:34:12 +0100

    update

--- app.js
+++ app.js
@@ -12,18 +12,39 @@ let getRecipe =(e)=>{
      // // Clear form
     searchBox.value='';
 
+    
+
     // to get value
     if(mySearch.trim()){
       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mySearch}`)
       .then(res=>res.json())
       .then(data => {
         console.log(data);
-        title.style.display='block';
-        title=searchTitle.innerHTML;
+        // title.style.display='block';
+        // title = myRecipe.innerHTML;
 
-        if(data.meals === null){
-          title.innerHTML =`<p> try aain<p>`;
+      // myRecipe.innerHTML =`<p>search result for '${mySearch}:</p>`;
+
+        if(data.meals ===null){
+          console.log('no result')
+          
+          nullResp.style.display = 'block';
+          myRecipe.innerHTML =search;
+
         }else{
+          myRecipe.innerHTML=data.meals.map(meal=>`
+          <div class ='img-meal'>
+          <img src = '${meal.strMealThumb}' alt ='${meal.strMeal}'/>
+         <div class='meal-desc' />
+         <h4>${meal.strMeal}</h4>.
+
+         </div>
+
+         </div>
+
+
+          </div>
+          `) .join('');
 
         }
 
@@ -33,28 +54,26 @@ let getRecipe =(e)=>{
       checkValue.classList.add("error");
       console.log('please type in a meal');
       checkValue.innerHTML ='please type in a meal';
-      
-
     }
-
-    // trying ternary operator
-    // (mySearch.trim() ? "fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mySearch}`)"
-    // .then(res=>res.json())
-    // .then(data => {console.log(data)}) : 'please enter a value');
-
 
 }
 
+// let enterSrch =()=>{
+//   mealBox.style.display = 'none';
+// }
+let reloadFunc = () => location.reload();
+
 // dom events
+const mealBox = document.getElementById('mealbox');
   const searchBox=document.getElementById('searchbox');
   const myBtn = document.getElementById('mybtn');
    const title =document.getElementById('title');
    const  myRecipe = document.getElementById('myrecipe');
    const checkValue = document.getElementById('check-value');
    const searchTitle =document.getElementById('searchtitle');
+   const search =document.getElementById('search');
+   const nullResp = document.getElementById('nully-resp');
 
-//event lkstener
-// myBtn.addEventListener('click', console.log('hi'));
-
+//event listener
 myBtn.addEventListener('click',  getRecipe);
      
